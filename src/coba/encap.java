cd660000268d13a2a782b25744dc283e246bc33a
MOKLET-1 <MOKLET-1@LAPTOP-HRD9F39E>
2020-12-11 08:35:24 +0700

    upload project

---
+++ src/coba/encap.java
@@ -0,0 +1,28 @@
+package coba;
+// parentclass
+public class encap {
+
+    // private = restricted access    
+    private String name; 
+    private int age;
+
+    // Getter
+    public String getName() {
+    return name;
+    }
+    // Setter
+    public void setName(String newName) {
+    this.name = newName;
+    }
+
+    // Getter
+    public int getAge() {
+    return age;
+    }
+    // Setter
+    public void setAge(int newAge) {
+    this.age = newAge;
+    }
+
+}
+
